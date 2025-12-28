2773b23446bb64426dc68ddd815937cfc0450e7b
TheodorosKon <theodoroskonstadinidis@gmail.com>
2025-12-28 18:07:22 +0100

    Basic MySQL Schema Commit
    Commit of the first schematic of the database with the tables that will be used. USER, ROLE, TICKET, COMMENT, ATTACHMENT, TICKET_TAG and TAG. Comments inside the file explain what each table is used for.

---
+++ schema.sql
@@ -0,0 +1,107 @@
+--Create the database if it does not exist
+CREATE DATABASE IF NOT EXISTS TicketingSystem
+
+--User table holds users of the system, this includes Support engineers, field or backoffice, Managers, Teamleaders
+--and anyone else that needs access to the system.
+CREATE TABLE USER (
+    user_id INT NOT NULL AUTO_INCREMENT,
+    username VARCHAR(255) COLLATE utf8mb4_unicode_ci NOT NULL UNIQUE,
+    email VARCHAR(255) COLLATE utf8mb4_unicode_ci NOT NULL UNIQUE,
+    password_hash VARCHAR(255) NOT NULL,
+    password_updated_at DATETIME NULL,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    phone_main VARCHAR(255) NOT NULL,
+    phone_backup VARCHAR(255),
+    is_active BOOLEAN,
+    role_id INT NOT NULL,
+    created_at DATETIME,
+    updated_at DATETIME,
+    PRIMARY KEY (user_id),
+    FOREIGN KEY (role_id) REFERENCES ROLE (role_id)
+);
+
+--Role table holds the roles each user has and will be used to determine what is visible and usable to each user.
+CREATE TABLE ROLE (
+    role_id INT NOT NULL AUTO_INCREMENT,
+    role_name VARCHAR(255) NOT NULL,
+    description VARCHAR(255),
+    created_at DATETIME
+    PRIMARY KEY (role_id)
+);
+
+--Ticket table holds info about each individual ticket.
+CREATE TABLE TICKET (
+    ticket_id INT NOT NULL AUTO_INCREMENT,
+    created_by INT NOT NULL,
+    assigned_to INT,
+    status VARCHAR(255),
+    priority INT,
+    title VARCHAR(255) NOT NULL,
+    description VARCHAR(255) NOT NULL,
+    device_manufacturer VARCHAR(255),
+    device_model VARCHAR(255),
+    device_serial_number VARCHAR(255) NOT NULL,
+    device_os VARCHAR(255),
+    opened_at DATETIME,
+    updated_at DATETIME,
+    closed_at DATETIME,
+    PRIMARY KEY (ticket_id),
+    FOREIGN KEY (created_by) REFERENCES USER (user_id),
+    FOREIGN KEY (assigned_to) REFERENCES USER (user_id)
+);
+
+--Attachment table holds any attachment that might have been uploaded by a support agent like photos or videos. It can also be used to hold SignOff documents.
+CREATE TABLE ATTACHMENT (
+    attachment_id INT NOT NULL AUTO_INCREMENT,
+    ticket_id INT NOT NULL,
+    file_name VARCHAR(255),
+    file_type VARCHAR(255),
+    file_size_bytes INT,
+    storage_url VARCHAR(255),
+    uploaded_at DATETIME,
+    uploaded_by INT,
+    PRIMARY KEY (attachment_id),
+    FOREIGN KEY (ticket_id) REFERENCES TICKET (ticket_id),
+    FOREIGN KEY (uploaded_by) REFERENCES USER (user_id)
+);
+
+--Comment table can hold comments left by the support or anyone that needs to leave extra information like a different phone number or address
+CREATE TABLE COMMENT (
+    comment_id INT NOT NULL AUTO_INCREMENT,
+    ticket_id INT NOT NULL,
+    user_id INT NOT NULL,
+    body TEXT NOT NULL,
+    created_at DATETIME
+    PRIMARY KEY (comment_id),
+    FOREIGN KEY (ticket_id) REFERENCES TICKET (ticket_id),
+    FOREIGN KEY (user_id) REFERENCES USER (user_id)
+);
+
+--Ticket tag holds TAGs that tickets might have. This can be used to categorize tickets and assign proper technicians to the tickets.
+--For example a ticket could have PRINTING and NETWORK tags which means the responding technician needs to have knowledge of both systems to be assigned.
+CREATE TABLE TICKET_TAG (
+    ticket_id INT NOT NULL,
+    tag_id INT NOT NULL,
+    ticket_id_tag_id INT NOT NULL AUTO_INCREMENT,
+    PRIMARY KEY (ticket_id_tag_id),
+    FOREIGN KEY (ticket_id) REFERENCES TICKET (ticket_id),
+    FOREIGN KEY (tag_id) REFERENCES TAG (tag_id)
+);
+
+--User tag holds tags that shows what skill each person has to be able to categorize them in certain situations.
+CREATE TABLE USER_TAG (
+    user_id INT NOT NULL,
+    tag_id INT NOT NULL,
+    user_id_tag_id INT NOT NULL AUTO_INCREMENT,
+    PRIMARY KEY (user_id_tag_id),
+    FOREIGN KEY (user_id) REFERENCES USER (user_id),
+    FOREIGN KEY (tag_id) REFERENCES TAG (tag_id)
+);
+
+--This holds the different tags that can exist.
+CREATE TABLE TAG (
+    tag_id INT NOT NULL AUTO_INCREMENT,
+    tag_name VARCHAR(255),
+    PRIMARY KEY (tag_id)
+);
